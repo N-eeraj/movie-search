5e9ba2486065e275f307c583314aa97a447b1685
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-21 18:56:54 +0530

    added responsive seachbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 
 function App() {
 
-  let apiKey = "a6326b75";
+  // apiKey = "a6326b75";
 
   return (
     <div className="App">
+      
       <h1 className="title">Movie Data Search</h1>
+
+      <input type="search" placeholder="Enter Film Name" className="search-bar"/>
+
     </div>
   );
 }
