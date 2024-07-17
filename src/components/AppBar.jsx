caecead2b4644f750544b5c2ca02ea7148e3b819
N-eeraj <neeraj7rajeena@gmail.com>
2024-07-17 16:20:46 +0530

    Updated app bar to single select type & UI updates

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -16,19 +16,19 @@ import { FaFilter, FaCheck } from 'react-icons/fa6'
 import Button from '@components/Button'
 
 const types = [
-  'Movie',
-  'Series',
-  'Episode',
+  'movie',
+  'series',
+  'episode',
 ]
 
 const AppBar = ({ onSearch }) => {
   const [searchParams, setSearchParams] = useSearchParams({
     q: 'Dragon Ball',
-    t: JSON.stringify(types),
+    t: 'movie',
   })
 
   const query = searchParams.get('q') ?? ''
-  const selectedTypes = JSON.parse(searchParams.get('t')) ?? []
+  const selectedType = searchParams.get('t') ?? ''
 
   const handleUpdateQuery = (query, value) => {
     setSearchParams(prev => {
@@ -40,7 +40,7 @@ const AppBar = ({ onSearch }) => {
   const handleSubmit = event => {
     event?.preventDefault()
     if (query)
-      onSearch({ query, types: selectedTypes.map(type => type.toLowerCase()) })
+      onSearch({ query, types })
   }
 
   useEffect(() => {
@@ -48,11 +48,10 @@ const AppBar = ({ onSearch }) => {
   }, [])
 
   return (
-    <div className="flex justify-end gap-x-3 md:py-4 md:px-7">
+    <div className="sticky top-0 flex justify-end gap-x-3 py-4 md:px-7 bg-primary z-10">
       <Listbox
-        value={selectedTypes}
-        multiple
-        onChange={value => value.length && handleUpdateQuery('t', JSON.stringify(value))}>
+        value={selectedType}
+        onChange={value => handleUpdateQuery('t', value)}>
         <ListboxButton as={Button}>
           <FaFilter />
         </ListboxButton>
@@ -62,12 +61,14 @@ const AppBar = ({ onSearch }) => {
             gap: 7,
           }}
           transition
-          className="rounded-xl border border-white/5 bg-white/5 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0">
+          className="rounded-xl border border-white/25 bg-black/5 p-1 backdrop-blur-lg [--anchor-gap:var(--spacing-1)] focus:outline-none transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0 z-10">
           {types.map(type => (
-            <ListboxOption key={type} value={type} className="group flex items-center gap-2 py-1.5 px-3 text-white text-sm/6 rounded-lg cursor-pointer data-[focus]:bg-white/10">
+            <ListboxOption key={type} value={type} className="group flex items-center gap-2 py-1.5 px-3 text-white text-sm/6 rounded-lg cursor-pointer data-[focus]:bg-black/20">
               <FaCheck className="invisible size-4 fill-white group-data-[selected]:visible" />
               <div className="text-sm/6 text-white">
-                {type}
+                {/* capitalizing lowercase "type" string */}
+                {type.charAt(0).toUpperCase()}
+                {type.slice(1)}
               </div>
             </ListboxOption>
           ))}
@@ -78,7 +79,7 @@ const AppBar = ({ onSearch }) => {
         <Input
           value={query}
           type="search"
-          placeholder={`Search ${selectedTypes.join(', ')}`}
+          placeholder={`Search ${selectedType}`}
           className="flex-1 py-1.5 px-3 text-white bg-white/5 border-none rounded-md ring-1 ring-white/10 focus:outline-none"
           onChange={({ target }) => handleUpdateQuery('q', target.value)} />
         <Button type="submit">
--- src/components/Movie/Card.jsx
+++ src/components/Movie/Card.jsx
@@ -1,28 +1,7 @@
-// react icon imports
-import { BiSolidCameraMovie } from 'react-icons/bi'
-import { PiTelevisionSimpleFill } from 'react-icons/pi'
-import { BsFillPlayBtnFill } from 'react-icons/bs'
-
 const Card = ({ Poster, Title, Year, Type }) => {
-  let Icon
-  switch (Type) {
-    case 'movie':
-      Icon = BiSolidCameraMovie
-      break;
-    case 'series':
-      Icon = PiTelevisionSimpleFill
-      break;
-    case 'episode':
-      Icon = BsFillPlayBtnFill
-      break;
-  }
-
   return (
     <article className="flex flex-col gap-y-1">
-      <picture className="relative min-h-40">
-        <img src={Poster} alt={Title} className="rounded-md object-contain bg-top" />
-        <Icon className="absolute top-1 left-1 size-6 bg-accent fill-primary p-1 rounded-full" />
-      </picture>
+      <img src={Poster} alt={Title} className="min-h-40 rounded-md object-contain bg-top" />
       <strong className="text-light">
         {Title}
       </strong>
